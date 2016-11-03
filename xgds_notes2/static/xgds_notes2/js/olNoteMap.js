8335954e51f3c14a3a96bbdc08ba22d5068060f9
Tamar Cohen <tamar.cohen@nasa.gov>
2016-11-02 18:42:32 -0700

    code to handle selection

--- xgds_notes2/static/xgds_notes2/js/olNoteMap.js
+++ xgds_notes2/static/xgds_notes2/js/olNoteMap.js
@@ -1,24 +1,40 @@
 // render json note information on the openlayers map
 
 var Note = {
+		selectedStylePath: '/static/xgds_notes2/icons/post_office_selected.png',
+		stylePath: '/static/xgds_notes2/icons/post_office.png',
         initStyles: function() {
             if (_.isUndefined(this.styles)){
                 this.styles = {};
                 this.styles['iconStyle'] = new ol.style.Style({
+                	zIndex: 1,
                     image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-                        src: '/static/xgds_notes2/icons/post_office.png',
+                        src: this.stylePath,
                         scale: 0.25
                         }))
                       });
+                this.styles['selectedIconStyle'] = new ol.style.Style({
+                	zIndex: 10,
+                    image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+                        src: this.selectedStylePath,
+                        scale: 0.25
+                        }))
+                      });
+                this.styles['yellowStroke'] =  new ol.style.Stroke({
+                    color: 'yellow',
+                    width: 2
+                });
+                this.styles['greenStroke'] =  new ol.style.Stroke({
+                    color: 'green',
+                    width: 2
+                });
                 this.styles['text'] = {
+                	zIndex: 1,
                     font: '12px Calibri,sans-serif',
                     fill: new ol.style.Fill({
                         color: 'black'
                     }),
-                    stroke: new ol.style.Stroke({
-                        color: 'yellow',
-                        width: 2
-                    }),
+                    stroke: this.styles['yellowStroke'],
                     offsetY: -15
                 };
             }
@@ -30,13 +46,13 @@ var Note = {
             this.initStyles();
             var olFeatures = [];
             for (var i = 0; i < notesJson.length; i++) {
-                if (notesJson[i].lat !== "") {
+                if (_.isNumber(notesJson[i].lat)) {
                     var noteFeature = this.constructMapElement(notesJson[i]);
                     olFeatures = olFeatures.concat(noteFeature);
                 }
             }
             var vectorLayer = new ol.layer.Vector({
-                name: "Notes",
+                name: notesJson[0].type,
                 source: new ol.source.Vector({
                     features: olFeatures
                 }),
@@ -47,8 +63,11 @@ var Note = {
         	noteJson.flattenedTags = '';
             var coords = transform([noteJson.lon, noteJson.lat]);
             var feature = new ol.Feature({
+            	selected: false,
                 name: getLocalTimeString(noteJson.event_time, noteJson.event_timezone),
-                uuid: noteJson.id,
+                uuid: noteJson.pk,
+                pk: noteJson.pk,
+                type: noteJson.type,
                 geometry: new ol.geom.Point(coords)
             });
             feature.setStyle(this.getStyles(noteJson));
@@ -59,9 +78,11 @@ var Note = {
             var styles = [this.styles['iconStyle']];
             if (noteJson.tags != '') {
                 var theText = new ol.style.Text(this.styles['text']);
-                noteJson.flattenedTags = noteJson.tag_slugs.reduce(function(a, b) {
-                    return a.concat(b + " ");
-                  });
+                if (noteJson.tag_names.length > 0){
+                	noteJson.flattenedTags = noteJson.tag_names.reduce(function(a, b) {
+                		return a.concat(" " + b);
+                	});
+                }
                 theText.setText(noteJson.flattenedTags);
                 var textStyle = new ol.style.Style({
                     text: theText
@@ -70,6 +91,26 @@ var Note = {
             }
             return styles;
         },
+        selectMapElement:function(feature){
+        	feature.selected = false;
+        	var styles = feature.getStyle();
+        	var newstyles = [this.styles['selectedIconStyle']];
+        	var newtextstyle = styles[1];
+        	newtextstyle.getText().setStroke(this.styles['greenStroke']);
+        	newtextstyle.setZIndex(10);
+        	newstyles.push(newtextstyle);
+        	feature.setStyle(newstyles);
+        },
+        deselectMapElement:function(feature){
+        	feature.selected = true;
+        	var styles = feature.getStyle();
+        	var newstyles = [this.styles['iconStyle']];
+        	var newtextstyle = styles[1];
+        	newtextstyle.getText().setStroke(this.styles['yellowStroke']);
+        	newtextstyle.setZIndex(1);
+        	newstyles.push(newtextstyle);
+        	feature.setStyle(newstyles);
+        },
         setupPopup: function(feature, noteJson) {
             var trString = "<tr><td>%s</td><td>%s</td></tr>";
             var formattedString = "<table>";
@@ -77,11 +118,8 @@ var Note = {
                 formattedString = formattedString + trString;
             }
             formattedString = formattedString + "</table>";
-            var data = ["Tags:", noteJson.tag_names,
+            var data = ["Tags:", noteJson.flattenedTags,
                         "Note:", noteJson.content,
-//                        "Altitude:", noteJson.altitude + " m",
-//                        "Time:", noteJson.event_time,
-//                        "Flight:", noteJson.flight,
                         "Author:", noteJson.author_name];
             var popupContents = vsprintf(formattedString, data);
             feature['popup'] = popupContents;
